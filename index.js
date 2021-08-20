73312d5c393009427d3830d1d0b6f0c83ed5ec5d
md-rakib420 <rayhan.senbag.bd@gmail.com>
2021-08-20 21:36:18 +0600

    final

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+//go to next inputfeild
+function pressEnterForNext() {
+    document.getElementById('user-email').addEventListener('keyup', function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById('user-password').focus();
+        }
+    });
+}
+
+//log in by enter key
+function pressEnterForLogIn() {
+    document.getElementById('user-password').addEventListener('keyup', function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById('login-button').click();
+        }
+    });
+}
+
+pressEnterForLogIn();
+pressEnterForNext();
 
 //index.html page
 document.getElementById('login-button').addEventListener('click', function () {
